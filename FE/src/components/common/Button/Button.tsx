57048bb67caf2a5b84a588816f18c37e2e57ca90
제동균 <donggyunje@gmail.com>
2025-03-17 23:42:08 +0900

    Fix : button onClick 속성 추가

--- FE/src/components/common/Button/Button.tsx
+++ FE/src/components/common/Button/Button.tsx
@@ -1,11 +1,11 @@
-import { ButtonType } from '@/types/Button'
-import { Icon } from './Icon'
-import { iconStyle } from './ButtonStyle'
+import { ButtonType } from '@/types/Button';
+import { Icon } from './Icon';
+import { iconStyle } from './ButtonStyle';
 
 // 두 가지 버전의 버튼 사용 가능
 // <Button icon='send' name='보내기' color='blue'>
 // <Button name='보내기' color='blue'>
-function Button({ icon, name, color }: ButtonType) {
+function Button({ icon, name, color, onClick }: ButtonType) {
   return (
     <>
       {/* 버튼을 감싸는 div의 크기를 지정해주면 너비가 가득 차지함 */}
@@ -23,15 +23,15 @@ function Button({ icon, name, color }: ButtonType) {
           md:pt-[13px] md:pb-[13px] md:pl-[16px] md:pr-[16px]
           lg:pt-[13px] lg:pb-[13px] lg:pl-[24px] lg:pr-[24px]
         `}
-      >
+        onClick={onClick}>
         <Icon icon={icon} />
 
         {/* name 값이 없을 때 여백 뜨지 않게 */}
-        {name &&
+        {name && (
           <span className="text-subtitle-1-sm md:text-subtitle-1-md lg:text-subtitle-1-lg font-p-500">
             {name}
           </span>
-        }
+        )}
       </button>
     </>
   );
--- FE/src/types/Button.ts
+++ FE/src/types/Button.ts
@@ -3,6 +3,7 @@ export interface ButtonType {
   icon?: string
   name?: string
   color: string
+  onClick?: () => void
 }
 
 // 아이콘 스타일 전용 props
