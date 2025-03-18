53ce69679ba404cbd97cdda8dae93f2222c55121
김미경 <rmfosem613@gmail.com>
2025-03-19 00:05:14 +0900

    Feat: 텍스트 편지 색 변경
    편지 색상 설정에 따라 편지 계절 템플릿 변경
    
    S12P21D201-91

--- FE/src/components/letter/LetterContent.tsx
+++ FE/src/components/letter/LetterContent.tsx
@@ -1,16 +1,64 @@
+// 이미지 import
 // summer
 import turtle from '@/assets/letter/summer-turtle.svg';
 import seashell from '@/assets/letter/summer-seashell.svg';
 
+// spring
+import cherry from '@/assets/letter/spring-cherry.svg';
+import sweet from '@/assets/letter/sping-sweet.svg';
 
+// autumn
+import plant from '@/assets/letter/autumn-plant.svg';
+import tree from '@/assets/letter/autumn-tree.svg';
+
+//winter
+import snow from '@/assets/letter/winter-snow.svg';
+import snowman from '@/assets/letter/winter-snowman.svg';
+
+import { useLetterStore } from '@/stores/useLetterStore';
 
 function LetterContent() {
+  const { selectedColor } = useLetterStore();
+
+  // 계절별 이미지 설정 (위치, 크기 등 포함)
+  const getSeasonalImages = () => {
+    switch (selectedColor) {
+      case 'spring':
+        return (
+          <>
+            <img src={cherry} className="absolute top-[10px] right-[10px] z-0 w-[110px] lg:w-[130px] md:w-[120px]" />
+            <img src={sweet} className="absolute bottom-0 left-0 z-0 w-[140px] lg:w-[160px] md:w-[150px]" />
+          </>
+        );
+      case 'summer':
+        return (
+          <>
+            <img src={seashell} className="absolute top-[10px] left-[10px] z-0 w-[90px] lg:w-[110px] md:w-[110px]" />
+            <img src={turtle} className="absolute bottom-[0px] right-[10px] z-0 w-[180px] lg:w-[200px] md:w-[190px]" />
+          </>
+        );
+      case 'autumn':
+        return (
+          <>
+            <img src={plant} className="absolute bottom-0 left-0 z-0 w-[140px] lg:w-[160px] md:w-[150px]" />
+            <img src={tree} className="absolute bottom-0 right-0 z-0 w-[250px] lg:w-[270px] md:w-[260px]" />
+          </>
+        );
+      case 'winter':
+      default:
+        return (
+          <>
+            <img src={snow} className="absolute bottom-[-70px] left-[-60px] z-0 w-[330px] lg:w-[350px] md:w-[340px]" />
+            <img src={snowman} className="absolute bottom-[-30px] right-[10px] z-0 w-[130px] lg:w-[250px] md:w-[140px]" />
+          </>
+        );
+    }
+  };
 
   return (
     <div className="relative border-2 border-gray-300 rounded-[8px] p-5 overflow-hidden">
       {/* 편지지 데코레이션: z-0 */}
-      <img src={seashell} className="absolute top-[10px] left-[10px] z-0" />
-      <img src={turtle} className="absolute bottom-[10px] right-[10px] z-0 w-[160px] lg:w-[180px] md:w-[170px] " />
+      {getSeasonalImages()}
 
       <div className="relative z-10 h-full flex flex-col">
         <div>
@@ -20,7 +68,7 @@ function LetterContent() {
           </div>
         </div>
 
-        {/* 편지지 줄 9개 추가 */}
+        {/* 편지지 줄 7개 추가 */}
         {[...Array(7)].map((_, index) => (
           <div key={index}>
             {/* border-t : 굵기 0.5px */}
