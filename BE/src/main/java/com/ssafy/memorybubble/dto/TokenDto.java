9d0de80c3bd9285b058cf2efc1e9e2757adc003f
김민주 <mintminju130@naver.com>
2025-03-18 17:16:43 +0900

    Feat: RefreshToken 객체 및 Dto, 서비스, 리포지토리 추가
    - redis에 저장되는 RefreshToken 객체 추가
    - AccessToken, RefreshToken 포함하는 TokenDto 추가
    - 서비스, 리포지토리 추가
    
    S12P21D201-89

---
+++ BE/src/main/java/com/ssafy/memorybubble/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.memorybubble.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+}
