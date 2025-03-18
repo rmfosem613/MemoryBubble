40ad9eea4a6496592b496396ed35d69393d37aed
김은영 <kimeunyoung-0425@naver.com>
2025-03-19 03:47:53 +0900

    Feat: CalendarPage 임시 Layout 설계 S12P21D201-67

--- FE/src/pages/CalendarPage.tsx
+++ FE/src/pages/CalendarPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import CalendarHeader from '@/components/calendar/CalendarHeader';
+import Calendar from '@/components/calendar/Calendar';
+import CalendarEvent from '@/components/calendar/CalendarEvent';
 
 function CalendarPage() {
-  return <div>CalendarPage</div>;
+  return (
+    <div className="pt-[65px] h-screen flex space-x-3 justify-center items-center">
+      <CalendarHeader />
+      <div className="min-w-[1024px]">
+        <Calendar />
+      </div>
+      <CalendarEvent />
+    </div>
+  );
 }
 
 export default CalendarPage;
