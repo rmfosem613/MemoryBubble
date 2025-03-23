b2c06bece23c7ee61a5fc7dc9ef3d1a1c2491480
김민주 <mintminju130@naver.com>
2025-03-23 22:21:42 +0900

    Feat: 앨범 연결 dto 추가
    S12P21D201-132

---
+++ BE/src/main/java/com/ssafy/memorybubble/dto/LinkRequest.java
@@ -0,0 +1,12 @@
+package com.ssafy.memorybubble.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class LinkRequest {
+    Long albumId;
+}
