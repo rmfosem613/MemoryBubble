843cbd0a1dd3065de8a64c283eea191e8cc859bf
김민주 <mintminju130@naver.com>
2025-03-24 01:57:37 +0900

    Feat: 일정 목록 반환 구현
    - 가족 id, 년, 월을 입력받고 조건에 맞는 일정 목록을 반환
    
    S12P21D201-134

--- BE/src/main/java/com/ssafy/memorybubble/api/schedule/service/ScheduleService.java
+++ BE/src/main/java/com/ssafy/memorybubble/api/schedule/service/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.ssafy.memorybubble.api.schedule.service;
 
 import com.ssafy.memorybubble.api.album.service.AlbumService;
+import com.ssafy.memorybubble.api.schedule.dto.ScheduleResponse;
 import com.ssafy.memorybubble.domain.Album;
 import com.ssafy.memorybubble.domain.Family;
 import com.ssafy.memorybubble.domain.Schedule;
@@ -16,6 +17,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+
 import static com.ssafy.memorybubble.common.exception.ErrorCode.*;
 
 @Service
@@ -28,7 +33,7 @@ public class ScheduleService {
     private final AlbumService albumService;
 
     @Transactional
-    public void addSchedule(Long userId, ScheduleRequest scheduleRequest) {
+    public ScheduleResponse addSchedule(Long userId, ScheduleRequest scheduleRequest) {
         log.info("schedule request: {}", scheduleRequest);
 
         User user = userService.getUser(userId);
@@ -69,6 +74,14 @@ public class ScheduleService {
 
         scheduleRepository.save(schedule);
         log.info("schedule: {}", schedule);
+
+        return ScheduleResponse.builder()
+                .scheduleId(schedule.getId())
+                .scheduleContent(schedule.getContent())
+                .startDate(schedule.getStartDate())
+                .endDate(schedule.getEndDate())
+                .albumId(schedule.getAlbum() != null ? schedule.getAlbum().getId() : null)
+                .build();
     }
 
     @Transactional
@@ -90,7 +103,7 @@ public class ScheduleService {
     }
 
     @Transactional
-    public void updateSchedule(Long userId, Long scheduleId, ScheduleRequest scheduleRequest) {
+    public ScheduleResponse updateSchedule(Long userId, Long scheduleId, ScheduleRequest scheduleRequest) {
         log.info("scheduleRequest: {}", scheduleRequest);
 
         User user = userService.getUser(userId);
@@ -112,20 +125,28 @@ public class ScheduleService {
         }
 
         // 시작 날짜만 수정하는 경우 - 기존의 끝 날짜보다 늦으면 예외처리
-        if (scheduleRequest.getStartDate() != null && scheduleRequest.getStartDate().isAfter(schedule.getEndDate())){
+        if (scheduleRequest.getStartDate() != null && scheduleRequest.getEndDate() == null && scheduleRequest.getStartDate().isAfter(schedule.getEndDate())){
             throw new ScheduleException(SCHEDULE_DATE_INVALID);
         }
 
         // 끝 날짜만 수정하는 경우 - 기존의 시작 날짜보다 이르면 예외 처리
-        if(scheduleRequest.getEndDate() != null && scheduleRequest.getEndDate().isBefore(schedule.getStartDate())){
+        if(scheduleRequest.getStartDate() == null && scheduleRequest.getEndDate() != null && scheduleRequest.getEndDate().isBefore(schedule.getStartDate())){
             throw new ScheduleException(SCHEDULE_DATE_INVALID);
         }
 
         schedule.update(scheduleRequest.getStartDate(), scheduleRequest.getEndDate(), scheduleRequest.getContent());
+
+        return ScheduleResponse.builder()
+                .scheduleId(schedule.getId())
+                .scheduleContent(schedule.getContent())
+                .startDate(schedule.getStartDate())
+                .endDate(schedule.getEndDate())
+                .albumId(schedule.getAlbum() != null ? schedule.getAlbum().getId() : null)
+                .build();
     }
 
     @Transactional
-    public void linkSchedule(Long userId, Long scheduleId, Long albumId) {
+    public ScheduleResponse linkSchedule(Long userId, Long scheduleId, Long albumId) {
         User user = userService.getUser(userId);
         log.info("user: {}", user);
 
@@ -150,5 +171,45 @@ public class ScheduleService {
             }
             schedule.update(album);
         }
+
+        return ScheduleResponse.builder()
+                .scheduleId(schedule.getId())
+                .scheduleContent(schedule.getContent())
+                .startDate(schedule.getStartDate())
+                .endDate(schedule.getEndDate())
+                .albumId(schedule.getAlbum() != null ? schedule.getAlbum().getId() : null)
+                .build();
+    }
+
+    public List<ScheduleResponse> getSchedules(Long userId, Long familyId, int year, int month) {
+        User user = userService.getUser(userId);
+        log.info("user: {}", user);
+
+        // user가 다른 그룹에 가입 되어있거나 가입되어 있지 않은 경우 예외 반환
+        Family family = user.getFamily();
+        if (family == null || !family.getId().equals(familyId)) {
+            throw new FamilyException(FAMILY_NOT_FOUND);
+        }
+        log.info("family: {}", family);
+
+        // 해당 월의 마지막 시작 날짜, 마지막 날짜 구함
+        YearMonth yearMonth = YearMonth.of(year, month);
+        LocalDate start = yearMonth.atDay(1);
+        LocalDate end = yearMonth.atEndOfMonth();
+
+        List<Schedule> schedules = scheduleRepository.findSchedules(familyId, start, end);
+        return schedules.stream()
+                .map(this::convertToScheduleResponse)
+                .toList();
+    }
+
+    public ScheduleResponse convertToScheduleResponse(Schedule schedule) {
+        return ScheduleResponse.builder()
+                .scheduleId(schedule.getId())
+                .startDate(schedule.getStartDate())
+                .endDate(schedule.getEndDate())
+                .scheduleContent(schedule.getContent())
+                .albumId(schedule.getAlbum() != null ? schedule.getAlbum().getId() : null)
+                .build();
     }
 }
