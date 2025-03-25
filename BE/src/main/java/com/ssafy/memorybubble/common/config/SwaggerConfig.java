a970586c7ca7fdd83f6911d4c507d8a98a70245d
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-03-25 14:56:19 +0900

    Feat: Swagger 서버 목록 추가

--- BE/src/main/java/com/ssafy/memorybubble/common/config/SwaggerConfig.java
+++ BE/src/main/java/com/ssafy/memorybubble/common/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.ssafy.memorybubble.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,10 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = {
+        @Server(url = "http://localhost:8080", description = "로컬 서버"),
+        @Server(url = "https://memorybubble.site", description = "개발 서버")
+})
 @Configuration
 public class SwaggerConfig {
     // Swagger UI: http://localhost:8080/swagger-ui/index.html
