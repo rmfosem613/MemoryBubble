0daac2e92910b9c17d205869e55e5ed2e888860d
김미경 <rmfosem613@gmail.com>
2025-03-25 14:58:15 +0900

    Feat: 그룹 생성 api 연동
    S12P21D201-152

--- FE/src/components/join/InputGroupName.tsx
+++ FE/src/components/join/InputGroupName.tsx
@@ -1,4 +1,8 @@
-function InputGroupName() {
+function InputGroupName({ onChangeGroupName, value }) {
+  const handleInputChange = (e) => {
+    onChangeGroupName(e.target.value);
+  };
+
   return (
     <div>
       <p className="mb-[10px] font-p-500 text-subtitle-1-lg">그룹명</p>
@@ -6,9 +10,11 @@ function InputGroupName() {
         type="text"
         placeholder="그룹명을 설정해주세요"
         className="w-full h-14 p-3 border placeholder-gray-400 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        value={value}
+        onChange={handleInputChange}
       />
     </div>
-  )
+  );
 }
 
-export default InputGroupName
+export default InputGroupName;
