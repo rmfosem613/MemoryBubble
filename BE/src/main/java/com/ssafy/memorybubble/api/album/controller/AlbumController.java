0a9894bd0703811a2de262805beb4639e79216a9
김민주 <mintminju130@naver.com>
2025-03-25 09:45:53 +0900

    Feat: 앨범 목록 조회 기능 구현
    - name이 없으면 사용자가 속한 가족의 모든 앨범 목록 반환
    - name이 있으면 사용자가 속한 가족 앨범 중 name이 포함된 앨범 목록 반환
    
    S12P21D201-177

--- BE/src/main/java/com/ssafy/memorybubble/api/album/controller/AlbumController.java
+++ BE/src/main/java/com/ssafy/memorybubble/api/album/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package com.ssafy.memorybubble.api.album.controller;
 
+import com.ssafy.memorybubble.api.album.dto.AlbumDto;
 import com.ssafy.memorybubble.api.album.dto.AlbumRequest;
 import com.ssafy.memorybubble.common.exception.ErrorResponse;
 import com.ssafy.memorybubble.api.album.service.AlbumService;
@@ -13,10 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/albums")
@@ -40,5 +40,20 @@ public class AlbumController {
                                          @RequestBody AlbumRequest albumRequest) {
         albumService.addAlbum(Long.valueOf(userDetails.getUsername()), albumRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    @Operation(
+            summary = "앨범 목록 조회 API",
+            description = "이름을 전달하면 이름이 포함된 앨범을 반환하고, 이름이 없으면 모든 앨범 목록을 반환합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공"),
+                    @ApiResponse(responseCode = "403", description = "해당 가족에 가입되어 있지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "토큰이 만료되었습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
+    public ResponseEntity<List<AlbumDto>> getAlbums(@AuthenticationPrincipal UserDetails userDetails,
+                                                    @RequestParam(value="name", required=false) String name) {
+        return ResponseEntity.ok(albumService.getAlbums(Long.valueOf(userDetails.getUsername()), name));
     }
 }
--- BE/src/main/java/com/ssafy/memorybubble/api/album/service/AlbumService.java
+++ BE/src/main/java/com/ssafy/memorybubble/api/album/service/AlbumService.java
@@ -1,5 +1,8 @@
 package com.ssafy.memorybubble.api.album.service;
 
+import com.ssafy.memorybubble.api.album.dto.AlbumDto;
+import com.ssafy.memorybubble.api.file.service.FileService;
+import com.ssafy.memorybubble.api.photo.repository.PhotoRepository;
 import com.ssafy.memorybubble.domain.Album;
 import com.ssafy.memorybubble.domain.Family;
 import com.ssafy.memorybubble.domain.User;
@@ -12,6 +15,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.ssafy.memorybubble.common.exception.ErrorCode.ALBUM_NOT_FOUND;
 import static com.ssafy.memorybubble.common.exception.ErrorCode.FAMILY_NOT_FOUND;
@@ -22,7 +30,9 @@ import static com.ssafy.memorybubble.common.exception.ErrorCode.FAMILY_NOT_FOUND
 @Slf4j
 public class AlbumService {
     private final AlbumRepository albumRepository;
+    private final PhotoRepository photoRepository;
     private final UserService userService;
+    private final FileService fileService;
 
     // 가족 생성 후 기본 앨범 생성
     @Transactional
@@ -66,7 +76,31 @@ public class AlbumService {
         albumRepository.save(album);
     }
 
+    public List<AlbumDto> getAlbums(Long userId, String name) {
+        User user = userService.getUser(userId);
+        Family family = user.getFamily();
+
+        // name이 없거나 빈 문자열이면 family로 album을 찾고 name이 있으면 포함된 album 찾음
+        List<Album> albums = StringUtils.hasText(name)
+                ? albumRepository.findByFamilyAndNameContaining(family, name)
+                : albumRepository.findByFamily(family);
+        return albums.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
     public Album getAlbum(Long id) {
         return albumRepository.findById(id).orElseThrow(()->new AlbumException(ALBUM_NOT_FOUND));
+    }
+
+    public AlbumDto convertToDto(Album album) {
+        return AlbumDto.builder()
+                .albumId(album.getId())
+                .albumName(album.getName())
+                .albumContent(album.getContent())
+                .backgroundColor(album.getBackgroundColor())
+                .thumbnailUrl(album.getThumbnail() == null ? null : fileService.getDownloadPresignedURL(album.getThumbnail()))
+                .photoLength(photoRepository.countByAlbumId(album.getId()))
+                .build();
     }
 }
