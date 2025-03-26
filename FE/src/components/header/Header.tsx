c3b58d2d3502a571c05206060f97ec95d67d41de
김은영 <kimeunyoung-0425@naver.com>
2025-03-26 10:39:30 +0900

    Feat: 그룹이름수정 모달 구현 GroupNameModal.tsx 파일 삭제 S12P21D201-139

--- FE/src/components/header/Header.tsx
+++ FE/src/components/header/Header.tsx
@@ -10,7 +10,8 @@ import {
   RotateCcw,
   Edit2,
 } from 'lucide-react';
-// import GroupNameModal from './GroupNameModal';
+import Modal from '../common/Modal/Modal';
+import useModal from '@/hooks/useModal';
 // import ProfileComponent from './ProfileModal';
 
 // 임시 데이터
@@ -48,23 +49,9 @@ const Header = () => {
   const [showInviteCode, setShowInviteCode] = useState(false);
   const [inviteCode, setInviteCode] = useState('A43DG650');
   const dropdownRef = useRef<HTMLDivElement>(null);
-  // const handleOpenProfileModal = () => {
-  //   openModal({
-  //     title: '내 프로필',
-  //     content: <ProfileComponent />,
-  //     confirmButtonText: '저장하기',
-  //     cancelButtonText: '취소하기',
-  //   });
-  // };
-
-  // const handleOpenGroupNameModal = () => {
-  //   openModal({
-  //     title: '그룹명 수정하기',
-  //     content: <GroupNameModal />,
-  //     confirmButtonText: '저장하기',
-  //     cancelButtonText: '취소하기',
-  //   });
-  // };
+  
+  const GroupNameEditModal = useModal();
+  const [newGroupName, setNewGroupName] = useState(family.familyName);
 
   // 드롭다운 외부 클릭 시 닫기
   useEffect(() => {
@@ -105,154 +92,127 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 h-[65px] w-full bg-white/70 backdrop-blur-sm flex items-center z-[45]">
-      <div className="container flex justify-between items-center">
-        <Link
-          to="/"
-          className="text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-800">
-          추억방울
-        </Link>
+    <>
+      <header className="fixed top-0 h-[65px] w-full bg-white/70 backdrop-blur-sm flex items-center z-[45]">
+        <div className="container flex justify-between items-center">
+          <Link
+            to="/"
+            className="text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-800">
+            추억방울
+          </Link>
 
-        {user.familyId && (
-          <div className="flex items-center space-x-3">
-            <Link
-              to="/font"
-              className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
-              나의 손글씨
-            </Link>
+          {user.familyId && (
+            <div className="flex items-center space-x-3">
+              <Link
+                to="/font"
+                className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
+                나의 손글씨
+              </Link>
 
-            <Link
-              to="/"
-              className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
-              추억 갤러리
-            </Link>
+              <Link
+                to="/"
+                className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
+                추억 갤러리
+              </Link>
 
-            <Link
-              to="/calendar"
-              className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
-              방울 캘린더
-            </Link>
+              <Link
+                to="/calendar"
+                className="p-1 rounded-full hover:bg-black/10 transition-colors text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg font-p-500">
+                방울 캘린더
+              </Link>
 
-            <Link
-              to="/storage"
-              className="relative p-1 rounded-full hover:bg-black/10 transition-colors">
-              <Mail
-                className="w-6 h-6 md:w-[26px] md:h-[26px] lg:w-7 lg:h-7"
-                strokeWidth={1.3}
-              />
-              {showLetter && (
-                <span className="absolute top-[5px] right-[2px]  h-2.5 w-2.5 bg-red-500 rounded-full"></span>
-              )}
-            </Link>
-
-            <div className="relative" ref={dropdownRef}>
-              <div
-                onClick={toggleDropdown}
-                className="relative h-9 w-9 md:h-10 md:w-10 lg:h-11 lg:w-11 rounded-full overflow-hidden flex justify-center items-center bg-gray-200 cursor-pointer hover:opacity-90 transition-opacity">
-                {family.thumbnailUrl ? (
-                  <img
-                    src={family.thumbnailUrl}
-                    alt="그룹 프로필 이미지"
-                    className="h-full w-full object-cover"
-                  />
-                ) : (
-                  <UserRound
-                    className="w-6 h-6 md:w-[26px] md:h-[26px] lg:w-7 lg:h-7"
-                    strokeWidth={1.3}
-                  />
+              <Link
+                to="/storage"
+                className="relative p-1 rounded-full hover:bg-black/10 transition-colors">
+                <Mail
+                  className="w-6 h-6 md:w-[26px] md:h-[26px] lg:w-7 lg:h-7"
+                  strokeWidth={1.3}
+                />
+                {showLetter && (
+                  <span className="absolute top-[5px] right-[2px]  h-2.5 w-2.5 bg-red-500 rounded-full"></span>
                 )}
-              </div>
+              </Link>
 
-              {/* 드롭다운 메뉴 */}
-              {isDropdownOpen && (
-                <div className="absolute right-0 mt-1 p-2 w-64 rounded-lg border shadow-md bg-white z-45">
-                  {/*  그룹 */}
-                  <div className="p-3 flex flex-col items-center space-y-2 bg-blue-50">
-                    {/* 그룹 이미지 */}
-                    {family.thumbnailUrl ? (
-                      <img
-                        src={family.thumbnailUrl}
-                        alt="그룹 이미지"
-                        className="h-24 w-24 rounded-full object-cover"
-                      />
-                    ) : (
-                      <div className="h-24 w-24 rounded-full border-2 border-blue-300 text-blue-300 flex items-center justify-center">
-                        <Image size={40} />
-                      </div>
-                    )}
+              <div className="relative" ref={dropdownRef}>
+                <div
+                  onClick={toggleDropdown}
+                  className="relative h-9 w-9 md:h-10 md:w-10 lg:h-11 lg:w-11 rounded-full overflow-hidden flex justify-center items-center bg-gray-200 cursor-pointer hover:opacity-90 transition-opacity">
+                  {family.thumbnailUrl ? (
+                    <img
+                      src={family.thumbnailUrl}
+                      alt="그룹 프로필 이미지"
+                      className="h-full w-full object-cover"
+                    />
+                  ) : (
+                    <UserRound
+                      className="w-6 h-6 md:w-[26px] md:h-[26px] lg:w-7 lg:h-7"
+                      strokeWidth={1.3}
+                    />
+                  )}
+                </div>
 
-                    {/* 그룹 이름 */}
-                    <div className="flex items-center space-x-2">
-                      <div className="text-h5-lg font-p-700">
-                        {family.familyName}
-                      </div>
-                      <div className="text-gray-500 hover:text-gray-700">
-                        <Edit2 size={16} />
-                      </div>
-                    </div>
-
-                    {/* 초대 코드 */}
-                    {!showInviteCode ? (
-                      <div
-                        className="p-2 w-full border border-gray-400 rounded-md text-center text-p-lg hover:bg-white/50 transition-colors cursor-pointer"
-                        onClick={() => setShowInviteCode(true)}>
-                        초대 코드 보기
-                      </div>
-                    ) : (
-                      <div className="p-2 w-full border border-gray-300 rounded-md text-p-lg flex justify-between items-center">
-                        <p>{inviteCode}</p>
-                        <div className="flex space-x-2">
-                          <div
-                            className="text-gray-500 hover:text-gray-700 cursor-pointer"
-                            onClick={copyInviteCode}>
-                            <Copy size={16} />
-                          </div>
-                          <div className="text-gray-500 hover:text-gray-700 cursor-pointer">
-                            <RotateCcw size={16} />
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-
-                  {/* 그룹 목록 */}
-                  <div className="max-h-60 overflow-y-auto mb-2">
-                    {/* 현재 사용자 */}
-                    <div className="flex items-center space-x-3 p-3 border-b border-gray-200">
-                      {user.profileUrl ? (
+                {/* 드롭다운 메뉴 */}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-1 p-2 w-64 rounded-lg border shadow-md bg-white z-45">
+                    {/*  그룹 */}
+                    <div className="p-3 flex flex-col items-center space-y-2 bg-blue-50">
+                      {/* 그룹 이미지 */}
+                      {family.thumbnailUrl ? (
                         <img
-                          src={user.profileUrl}
-                          alt="프로필 이미지"
-                          className="h-11 w-11 rounded-full object-cover"
+                          src={family.thumbnailUrl}
+                          alt="그룹 이미지"
+                          className="h-24 w-24 rounded-full object-cover"
                         />
                       ) : (
-                        <div className="h-11 w-11 rounded-full bg-gray-200 flex items-center justify-center">
-                          <UserRound />
+                        <div className="h-24 w-24 rounded-full border-2 border-blue-300 text-blue-300 flex items-center justify-center">
+                          <Image size={40} />
                         </div>
                       )}
-                      <div className="flex-1">
-                        <div className="flex justify-between">
-                          <p className="text-sm">{user.name}</p>
-                          <div className="text-gray-500 hover:text-gray-700">
-                            <Settings size={18} />
+
+                      {/* 그룹 이름 */}
+                      <div className="flex items-center space-x-2">
+                        <div className="text-h5-lg font-p-700">
+                          {family.familyName}
+                        </div>
+                        <div
+                          className="text-gray-500 hover:text-gray-700"
+                          onClick={GroupNameEditModal.open}>
+                          <Edit2 size={16} />
+                        </div>
+                      </div>
+
+                      {/* 초대 코드 */}
+                      {!showInviteCode ? (
+                        <div
+                          className="p-2 w-full border border-gray-400 rounded-md text-center text-p-lg hover:bg-white/50 transition-colors cursor-pointer"
+                          onClick={() => setShowInviteCode(true)}>
+                          초대 코드 보기
+                        </div>
+                      ) : (
+                        <div className="p-2 w-full border border-gray-300 rounded-md text-p-lg flex justify-between items-center">
+                          <p>{inviteCode}</p>
+                          <div className="flex space-x-2">
+                            <div
+                              className="text-gray-500 hover:text-gray-700 cursor-pointer"
+                              onClick={copyInviteCode}>
+                              <Copy size={16} />
+                            </div>
+                            <div className="text-gray-500 hover:text-gray-700 cursor-pointer">
+                              <RotateCcw size={16} />
+                            </div>
                           </div>
                         </div>
-                        <p className="text-xs text-gray-500">{user.birth}</p>
-                        <p className="text-xs text-gray-500">
-                          {user.phoneNumber}
-                        </p>
-                      </div>
+                      )}
                     </div>
 
-                    {/* 그룹 멤버 */}
-                    {family.familyMembers.map((member, index) => (
-                      <div
-                        key={index}
-                        className="flex items-center space-x-3 p-3 border-b border-gray-200">
-                        {member.profileUrl ? (
+                    {/* 그룹 목록 */}
+                    <div className="max-h-60 overflow-y-auto mb-2">
+                      {/* 현재 사용자 */}
+                      <div className="flex items-center space-x-3 p-3 border-b border-gray-200">
+                        {user.profileUrl ? (
                           <img
-                            src={member.profileUrl}
-                            alt={`${member.name} 프로필 이미지`}
+                            src={user.profileUrl}
+                            alt="프로필 이미지"
                             className="h-11 w-11 rounded-full object-cover"
                           />
                         ) : (
@@ -260,33 +220,88 @@ const Header = () => {
                             <UserRound />
                           </div>
                         )}
-                        <div>
-                          <p className="text-sm">{member.name}</p>
+                        <div className="flex-1">
+                          <div className="flex justify-between">
+                            <p className="text-sm">{user.name}</p>
+                            <div className="text-gray-500 hover:text-gray-700">
+                              <Settings size={18} />
+                            </div>
+                          </div>
+                          <p className="text-xs text-gray-500">{user.birth}</p>
                           <p className="text-xs text-gray-500">
-                            {member.birth}
-                          </p>
-                          <p className="text-xs text-gray-500">
-                            {member.phoneNumber}
+                            {user.phoneNumber}
                           </p>
                         </div>
                       </div>
-                    ))}
-                  </div>
 
-                  {/* 로그아웃 */}
-                  <button
-                    className="p-2 w-full flex items-center space-x-2 text-sm text-red-600 hover:text-red-700"
-                    onClick={() => {}}>
-                    <LogOut size={18} />
-                    <p>로그아웃</p>
-                  </button>
-                </div>
-              )}
+                      {/* 그룹 멤버 */}
+                      {family.familyMembers.map((member, index) => (
+                        <div
+                          key={index}
+                          className="flex items-center space-x-3 p-3 border-b border-gray-200">
+                          {member.profileUrl ? (
+                            <img
+                              src={member.profileUrl}
+                              alt={`${member.name} 프로필 이미지`}
+                              className="h-11 w-11 rounded-full object-cover"
+                            />
+                          ) : (
+                            <div className="h-11 w-11 rounded-full bg-gray-200 flex items-center justify-center">
+                              <UserRound />
+                            </div>
+                          )}
+                          <div>
+                            <p className="text-sm">{member.name}</p>
+                            <p className="text-xs text-gray-500">
+                              {member.birth}
+                            </p>
+                            <p className="text-xs text-gray-500">
+                              {member.phoneNumber}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+
+                    {/* 로그아웃 */}
+                    <button
+                      className="p-2 w-full flex items-center space-x-2 text-sm text-red-600 hover:text-red-700"
+                      onClick={() => {}}>
+                      <LogOut size={18} />
+                      <p>로그아웃</p>
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-    </header>
+          )}
+        </div>
+      </header>
+      <Modal
+        isOpen={GroupNameEditModal.isOpen}
+        onClose={GroupNameEditModal.close}
+        title="그룹명 수정"
+        confirmButtonText="저장하기"
+        cancelButtonText="취소하기"
+        onConfirm={() => {
+          // axios 요청 추가
+          setFamily({
+            ...family,
+            familyName: newGroupName,
+          });
+        }}>
+        <div className="mb-3">
+          <p className="pb-2">수정할 그룹명을 입력해주세요.</p>
+          <input
+            type="text"
+            value={newGroupName}
+            onChange={(e) => setNewGroupName(e.target.value)}
+            className="w-full py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+            placeholder="그룹명을 입력하세요"
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
 
