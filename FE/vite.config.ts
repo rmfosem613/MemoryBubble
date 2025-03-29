c9c58ea812c7cda2abe30a723c88045601e1ef5d
김미경 <rmfosem613@gmail.com>
2025-03-29 22:30:31 +0900

    Fix: 사용안하는 타입 제거

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath, URL } from 'url'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
