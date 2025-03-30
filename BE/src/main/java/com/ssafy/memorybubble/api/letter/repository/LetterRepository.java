15fa617286f271e06ca62f9358ea52ebdb5c9d41
김민주 <mintminju130@naver.com>
2025-03-30 20:09:24 +0900

    Comment: 안 쓰는 메소드 주석처리
    S12P21D201-197

--- BE/src/main/java/com/ssafy/memorybubble/api/letter/repository/LetterRepository.java
+++ BE/src/main/java/com/ssafy/memorybubble/api/letter/repository/LetterRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
-    List<Letter> findByReceiverId(Long receiverId);
+    // List<Letter> findByReceiverId(Long receiverId);
     @Query("SELECT l from Letter l JOIN FETCH l.sender WHERE l.receiver.id = :receiverId")
     List<Letter> findByReceiverIdWithSender(@Param("receiverId") Long receiverId);
     boolean existsByReceiverIdAndIsReadFalse(Long receiverId);
