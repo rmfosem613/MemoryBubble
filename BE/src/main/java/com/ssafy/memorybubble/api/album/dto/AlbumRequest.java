43cb970210ab1c16e51dc95b68595bb765d546fa
김민주 <mintminju130@naver.com>
2025-04-01 13:51:43 +0900

    Feat: 유효성 검증 추가
    S12P21D201-215

--- BE/src/main/java/com/ssafy/memorybubble/api/album/dto/AlbumRequest.java
+++ BE/src/main/java/com/ssafy/memorybubble/api/album/dto/AlbumRequest.java
@@ -1,5 +1,7 @@
 package com.ssafy.memorybubble.api.album.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,13 +11,16 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class AlbumRequest {
+    @NotNull
     Long familyId;
 
+    @NotBlank
     @Size(min = 1, max = 7, message = "앨범 이름은 1자 이상 7자 이하여야 합니다")
     String albumName;
 
     @Size(max = 60, message = "앨범 내용은 60자 이하여야 합니다")
     String albumContent;
 
+    @NotBlank
     String backgroundColor;
 }
