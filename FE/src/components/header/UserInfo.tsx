407657d2c50202d6db4911f670fe83ea7775bca3
김은영 <kimeunyoung-0425@naver.com>
2025-04-03 15:52:09 +0900

    Refactor: Header의 UserInfo 컴포넌트 상태 및 함수 분리 S12P21D201-199

--- FE/src/components/header/UserInfo.tsx
+++ FE/src/components/header/UserInfo.tsx
@@ -1,26 +1,29 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Copy, RotateCcw, Edit2, Settings, LogOut } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useEffect } from 'react';
+import { Copy, X, Edit2, Settings, LogOut } from 'lucide-react';
 import useUserStore from '@/stores/useUserStore';
-import useUserApi from '@/apis/useUserApi';
 import useModal from '@/hooks/useModal';
+import useHeader from '@/hooks/useHeader';
 import GroupEditModal from './GroupEditModal';
 import ProfileEditModal from './ProfileEditModal';
 
 const UserInfo = () => {
-  const { user, family, resetUser } = useUserStore();
-  const { getFamilyInviteCode, logout } = useUserApi();
-  const navigate = useNavigate();
-
-  // 드롭다운 관련 상태
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [showInviteCode, setShowInviteCode] = useState(false);
-  const [inviteCode, setInviteCode] = useState('A43DG650');
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const { user, family } = useUserStore();
+  const {
+    isDropdownOpen,
+    setIsDropdownOpen,
+    toggleDropdown,
+    showInviteCode,
+    setShowInviteCode,
+    inviteCode,
+    handleShowInviteCode,
+    copyInviteCode,
+    handleLogout,
+  } = useHeader();
 
   // 모달 관련 상태
   const groupNameModal = useModal();
   const profileEditModal = useModal();
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   // 드롭다운 외부 클릭 시 닫기
   useEffect(() => {
@@ -43,97 +46,43 @@ const UserInfo = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [groupNameModal.isOpen, profileEditModal.isOpen]);
-
-  // 초대 코드 복사
-  const copyInviteCode = () => {
-    navigator.clipboard
-      .writeText(inviteCode)
-      .then(() => {
-        console.log('초대 코드 복사 성공');
-      })
-      .catch((err) => {
-        console.log('초대 코드 복사 실패');
-      });
-  };
-
-  const toggleDropdown = () => {
-    if (isDropdownOpen) {
-      setShowInviteCode(false);
-    }
-
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  // 초대 코드 보기 버튼 클릭 시 호출
-  const handleShowInviteCode = async () => {
-    try {
-      if (user.familyId) {
-        const response = await getFamilyInviteCode(user.familyId);
-        if (response.status === 200) {
-          setInviteCode(response.data.code);
-          setShowInviteCode(true);
-        }
-      }
-    } catch (error) {
-      console.error('초대 코드 가져오기 실패:', error);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      // 로그아웃 API 호출
-      const response = await logout();
-      if (response.status === 200) {
-        // 로컬 스토리지에서 토큰 삭제
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-
-        // 유저 스토어 상태 초기화
-        resetUser();
-
-        // 로그인 페이지로 이동
-        navigate('/kakao');
-      }
-    } catch (error) {
-      console.error('로그아웃 오류:', error);
-      // 오류가 발생해도 토큰 삭제 및 페이지 이동 처리
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('refreshToken');
-      resetUser();
-      navigate('/kakao');
-    }
-  };
+  }, [
+    groupNameModal.isOpen,
+    profileEditModal.isOpen,
+    setIsDropdownOpen,
+    setShowInviteCode,
+  ]);
 
   return (
     <>
       <div className="relative" ref={dropdownRef}>
-        <div
-          onClick={toggleDropdown}
-          className="relative h-9 w-9 md:h-10 md:w-10 lg:h-11 lg:w-11 rounded-full overflow-hidden flex justify-center items-center bg-gray-200 cursor-pointer hover:opacity-90 transition-opacity">
-          <img
-            src={user.profileUrl}
-            alt="프로필 이미지"
-            className="h-full w-full object-cover"
-          />
-        </div>
+        {/*  헤더 - 프로필 사진 */}
+        <img
+          src={user.profileUrl}
+          alt="프로필 이미지"
+          className="h-10 w-10 md:h-11 md:w-11 rounded-full object-cover bg-white cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleDropdown();
+          }}
+        />
 
         {/* 드롭다운 메뉴 */}
         {isDropdownOpen && (
-          <div className="absolute right-0 mt-1 p-2 w-64 rounded-lg border shadow-md bg-white z-45">
-            {/*  그룹 */}
+          <div className="absolute right-0 mt-1.5 p-2 w-64 rounded-lg shadow-md bg-white z-30">
+            {/* ----- 그룹 ----- */}
             <div className="p-3 flex flex-col items-center space-y-2 bg-blue-50">
               {/* 그룹 이미지 */}
               <img
                 src={family.thumbnailUrl}
                 alt="그룹 이미지"
-                className="h-24 w-24 rounded-full object-cover"
+                className="h-24 w-24 rounded-full object-cover bg-white"
               />
               {/* 그룹 이름 */}
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-1">
                 <div className="text-h5-lg font-p-700">{family.familyName}</div>
                 <div
-                  className="text-gray-500 hover:text-gray-700"
+                  className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
                   onClick={(e) => {
                     e.stopPropagation();
                     groupNameModal.open();
@@ -141,65 +90,74 @@ const UserInfo = () => {
                   <Edit2 size={16} />
                 </div>
               </div>
-
               {/* 초대 코드 */}
               {!showInviteCode ? (
                 <div
-                  className="p-2 w-full border border-gray-400 rounded-md text-center text-p-lg hover:bg-white/50 transition-colors cursor-pointer"
-                  onClick={handleShowInviteCode}>
+                  className="p-2 w-full border border-gray-400 rounded-md text-center text-p-lg hover:bg-white/50 transition-colors duration-200 cursor-pointer"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleShowInviteCode();
+                  }}>
                   초대 코드 보기
                 </div>
               ) : (
-                <div className="p-2 w-full border border-gray-300 rounded-md text-p-lg flex justify-between items-center">
+                <div className="py-2 px-3 w-full border border-gray-300 rounded-md text-p-lg flex justify-between items-center">
                   <p>{inviteCode}</p>
                   <div className="flex space-x-2">
                     <div
-                      className="text-gray-500 hover:text-gray-700 cursor-pointer"
-                      onClick={copyInviteCode}>
+                      className="text-gray-500 hover:text-gray-700 cursor-pointer transition-colors duration-200"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        copyInviteCode();
+                      }}>
                       <Copy size={16} />
                     </div>
-                    <div className="text-gray-500 hover:text-gray-700 cursor-pointer">
-                      <RotateCcw size={16} />
+                    <div
+                      className="text-gray-500 hover:text-gray-700 cursor-pointer transition-colors duration-200"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setShowInviteCode(false);
+                      }}>
+                      <X size={16} />
                     </div>
                   </div>
                 </div>
               )}
             </div>
 
-            {/* 그룹 목록 */}
-            <div className="max-h-60 overflow-y-auto mb-2">
+            {/* ----- 그룹 목록 ----- */}
+            <div className="max-h-60 overflow-y-auto mb-3">
               {/* 현재 사용자 */}
               <div className="flex items-center space-x-3 p-3 border-b border-gray-200">
                 <img
                   src={user.profileUrl}
                   alt="프로필 이미지"
-                  className="h-11 w-11 rounded-full object-cover"
+                  className="h-11 w-11 rounded-full object-cover bg-white"
                 />
                 <div className="flex-1">
                   <div className="flex justify-between">
                     <p className="text-sm">{user.name}</p>
                     <div
-                      className="text-gray-500 hover:text-gray-700"
+                      className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
                       onClick={(e) => {
                         e.stopPropagation();
                         profileEditModal.open();
                       }}>
-                      <Settings size={18} />
+                      <Settings size={20} />
                     </div>
                   </div>
                   <p className="text-xs text-gray-500">{user.birth}</p>
                   <p className="text-xs text-gray-500">{user.phoneNumber}</p>
                 </div>
               </div>
-
               {/* 그룹 멤버 */}
-              {family.familyMembers.map((member, index) => (
+              {family.familyMembers.map((member) => (
                 <div
-                  key={index}
+                  key={member.userId}
                   className="flex items-center space-x-3 p-3 border-b border-gray-200">
                   <img
                     src={member.profileUrl}
-                    alt={`프로필 이미지`}
+                    alt="프로필 이미지"
                     className="h-11 w-11 rounded-full object-cover"
                   />
                   <div>
@@ -215,9 +173,12 @@ const UserInfo = () => {
 
             {/* 로그아웃 */}
             <button
-              className="p-2 w-full flex items-center space-x-2 text-sm text-red-600 hover:text-red-700"
-              onClick={handleLogout}>
-              <LogOut size={18} />
+              className="px-2 pb-1 w-full flex items-center space-x-2 text-h-logo-sm text-red-600 hover:text-red-700 transition-colors duration-300"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleLogout();
+              }}>
+              <LogOut size={16} />
               <p>로그아웃</p>
             </button>
           </div>
--- FE/src/hooks/useHeader.ts
+++ FE/src/hooks/useHeader.ts
@@ -4,14 +4,19 @@ import useUserStore from '@/stores/useUserStore';
 import useUserApi from '@/apis/useUserApi';
 
 const useHeader = () => {
-  const { user, resetUser } = useUserStore(); // 스토어 관련 상태
-  const { checkUnreadLetter, logout } = useUserApi(); // api 관련
+  const { user, family, resetUser } = useUserStore(); // 스토어 관련 상태
+  const { checkUnreadLetter, logout, getFamilyInviteCode } = useUserApi(); // api 관련
   const location = useLocation(); // 현재 위치 정보 가져오기
 
   // 헤더 상태 및 검사
-  const [isUnread, setIsUnread] = useState(false)
+  const [isUnread, setIsUnread] = useState(false);
   const isLoggedIn = !!user.userId;
   const hasCompleteProfile = !!user.familyId && !!user.birth;
+
+  // UserInfo 컴포넌트에서 분리한 상태들
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [showInviteCode, setShowInviteCode] = useState(false);
+  const [inviteCode, setInviteCode] = useState('');
 
   // 읽지 않은 편지 확인 - 페이지 이동시마다 실행
   useEffect(() => {
@@ -33,7 +38,36 @@ const useHeader = () => {
         checkForUnreadLetters();
       }
     }
-  }, [checkUnreadLetter, setIsUnread, hasCompleteProfile, location.pathname]);
+  }, [checkUnreadLetter, hasCompleteProfile, location.pathname]);
+
+  // 사용자 정보 (드롭다운)
+  const toggleDropdown = useCallback(() => {
+    setShowInviteCode(false);
+    setIsDropdownOpen(!isDropdownOpen);
+  }, [isDropdownOpen]);
+
+  // 초대 코드 복사
+  const copyInviteCode = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(inviteCode);
+      console.log('초대 코드 복사 성공');
+    } catch (error) {
+      console.log('초대 코드 복사 실패');
+    }
+  }, [inviteCode]);
+
+  // 초대 코드 보기 버튼 클릭 시 호출
+  const handleShowInviteCode = useCallback(async () => {
+    try {
+      const response = await getFamilyInviteCode(user.familyId);
+      if (response.status === 200) {
+        setInviteCode(response.data.code);
+        setShowInviteCode(true);
+      }
+    } catch (error) {
+      console.error('초대 코드 가져오기 실패:', error);
+    }
+  }, [getFamilyInviteCode, user.familyId]);
 
   // 로그아웃 처리
   const handleLogout = useCallback(async () => {
@@ -61,6 +95,15 @@ const useHeader = () => {
     isLoggedIn,
     hasCompleteProfile,
     handleLogout,
+
+    isDropdownOpen,
+    setIsDropdownOpen,
+    toggleDropdown,
+    showInviteCode,
+    setShowInviteCode,
+    inviteCode,
+    handleShowInviteCode,
+    copyInviteCode,
   };
 };
 
