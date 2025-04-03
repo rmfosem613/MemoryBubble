4b58eff2d2d9a7a17bab0cd720e047838b488e50
김은영 <kimeunyoung-0425@naver.com>
2025-04-04 07:18:14 +0900

    Design: Header 컴포넌트 메뉴 통합 및 반응형 디자인 적용 S12P21D201-199

--- FE/src/components/header/Header.tsx
+++ FE/src/components/header/Header.tsx
@@ -1,51 +1,156 @@
 import { Link } from 'react-router-dom';
-import { LogOut } from 'lucide-react';
+import { X, Menu, Mail, LogOut, FileType, Image, Calendar } from 'lucide-react';
 import useHeader from '@/hooks/useHeader';
-import HeaderMenuItems from './HeaderMenuItems';
 import UserInfo from './UserInfo';
 import { useUserStatus } from '@/hooks/useUserSatus';
 
 const Header = () => {
-  const { handleLogout } = useHeader();
+  const {
+    isMenuOpen,
+    menuRef,
+    buttonRef,
+    isUnread,
+    isMainPath,
+    isActivePath,
+    toggleMenu,
+    closeMenu,
+    handleLogout,
+  } = useHeader();
+
   const { isLoggedIn, hasCompleteProfile } = useUserStatus();
 
-  return (
-    <header className="fixed top-0 h-[50px] md:h-[55px] lg:h-[60px] w-full bg-white/30 backdrop-blur-md flex items-center z-[50]">
-      <div className="container flex justify-between items-center">
-        {/* 항상 표시 - 로고 */}
-        <Link to="/intro">
-          <img
-            src="/logo-3.svg"
-            alt="추억방울"
-            className="h-8 md:h-[35px] lg:h-[38px]"
-          />
-        </Link>
+  const menuItems = [
+    { path: '/font', label: '나의 손글씨', icon: FileType },
+    { path: '/', label: '추억 갤러리', icon: Image },
+    { path: '/calendar', label: '방울 캘린더', icon: Calendar },
+  ];
 
-        {/* 로그인한 경우 */}
-        {isLoggedIn && (
-          <>
-            {/* 가족 및 프로필이 있는 경우 */}
-            {hasCompleteProfile ? (
-              <div className="flex items-center space-x-2">
-                <HeaderMenuItems className="flex items-center space-x-1 lg:space-x-2" />
-                <UserInfo />
-              </div>
-            ) : (
-              // 가족 및 프로필이 없는 경우 로그아웃만 표시
-              <button
-                className="flex items-center space-x-1 text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg whitespace-nowrap hover:text-red-700 transition-colors duration-300"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleLogout();
-                }}>
-                <LogOut className="size-4 md:size-[18px]" />
-                <p>로그아웃</p>
-              </button>
-            )}
-          </>
-        )}
-      </div>
-    </header>
+  const styles = {
+    menuItem:
+      'px-2 py-1.5 rounded-full hover:bg-black/5 transition-colors duration-300 text-h-logo-md lg:text-h-logo-lg font-p-500',
+    menuItemMobile:
+      'px-2 py-2 rounded-full hover:bg-black/5 transition-colors duration-300 text-h-logo-md font-p-300 flex space-x-2 items-center border-b border-black/5',
+    menuItemAtive: 'text-blue-500',
+  };
+
+  return (
+    <>
+      <header className="fixed top-0 h-[50px] md:h-[55px] lg:h-[60px] w-full bg-white/30 backdrop-blur-md flex items-center z-[50]">
+        <div className="container flex justify-between items-center">
+          {/* 항상 표시 - 로고 */}
+          <Link to="/intro" className="order-1 sm:order-none">
+            <img
+              src="/logo-3.svg"
+              alt="추억방울"
+              className="h-8 md:h-[35px] lg:h-[38px]"
+            />
+          </Link>
+
+          {/* 로그인한 경우 */}
+          {isLoggedIn && (
+            <>
+              {/* 가족 및 프로필이 있는 경우 */}
+              {hasCompleteProfile ? (
+                <>
+                  <div className="flex items-center space-x-2 order-2 sm:order-none">
+                    <div className="hidden sm:flex items-center space-x-1 lg:space-x-2">
+                      {menuItems.map((item) => (
+                        <Link
+                          key={item.path}
+                          to={item.path}
+                          className={`${styles.menuItem} ${isActivePath(item.path) ? styles.menuItemAtive : ''}`}>
+                          {item.label}
+                        </Link>
+                      ))}
+                      <Link
+                        to="/storage"
+                        className={`relative px-2 py-1 rounded-full hover:bg-black/5 transition-colors duration-300 group  ${isActivePath('/storage') || isActivePath('/letter') ? styles.menuItemAtive : ''}`}>
+                        <Mail
+                          className="size-7 lg:size-[30px]"
+                          strokeWidth={1.3}
+                        />
+                        {isUnread && (
+                          <>
+                            <span className="absolute top-[6px] right-[7px] h-2 w-2 bg-red-500 rounded-full"></span>
+                            <span className="invisible group-hover:visible absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 py-1 bg-gray-700 text-white text-xs rounded whitespace-nowrap">
+                              읽지 않은 편지가 있습니다
+                            </span>
+                          </>
+                        )}
+                      </Link>
+                    </div>
+                    <UserInfo />
+                  </div>
+
+                  {/* 모바일용 메뉴 버튼 */}
+                  <div
+                    className="sm:hidden relative flex items-center order-0 sm:order-none"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      toggleMenu();
+                    }}
+                    ref={buttonRef}>
+                    <button>
+                      {isMenuOpen ? (
+                        <X className="size-6" strokeWidth={1.5} />
+                      ) : (
+                        <Menu className="size-6" strokeWidth={1.5} />
+                      )}
+                    </button>
+                    {isUnread && !isMenuOpen && (
+                      <span className="absolute top-[-3px] right-[-7px] h-2 w-2 bg-red-500 rounded-full"></span>
+                    )}
+                  </div>
+                </>
+              ) : (
+                // 가족 및 프로필이 없는 경우 로그아웃만 표시
+                <button
+                  className="flex items-center space-x-1 text-h-logo-sm md:text-h-logo-md lg:text-h-logo-lg whitespace-nowrap hover:text-red-700 transition-colors duration-300"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleLogout();
+                  }}>
+                  <LogOut className="size-4 md:size-[18px]" />
+                  <p>로그아웃</p>
+                </button>
+              )}
+            </>
+          )}
+        </div>
+      </header>
+
+      {/* 모바일용 메뉴 */}
+      {isMenuOpen && (
+        <div
+          ref={menuRef}
+          className={`sm:hidden fixed top-[50px] left-0 w-full ${isMainPath() ? 'bg-white/30' : 'bg-white'} backdrop-blur-md z-[50] shadow-sm flex flex-col container space-y-2 py-3`}>
+          {menuItems.map((item) => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className={`${styles.menuItemMobile} ${isActivePath(item.path) ? styles.menuItemAtive : ''} `}
+              onClick={(e) => {
+                e.stopPropagation();
+                closeMenu();
+              }}>
+              <item.icon size={26} strokeWidth={1.3} />
+              <p>{item.label}</p>
+            </Link>
+          ))}
+          <Link
+            to="/storage"
+            className={`${styles.menuItemMobile} ${isActivePath('/storage') || isActivePath('/letter') ? styles.menuItemAtive : ''}`}>
+            <div className="relative">
+              <Mail size={26} strokeWidth={1.3} />
+              {isUnread && isMenuOpen && (
+                <span className="absolute top-[1px] right-[-1px] h-2 w-2 bg-red-500 rounded-full"></span>
+              )}
+            </div>
+            <p>편지 보관함</p>
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 
--- FE/src/hooks/useHeader.ts
+++ FE/src/hooks/useHeader.ts
@@ -1,11 +1,85 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import useUserStore from '@/stores/useUserStore';
 import useUserApi from '@/apis/useUserApi';
+import { useHasCompleteProfile } from './useUserSatus';
 
 const useHeader = () => {
-  const { resetUser } = useUserStore(); 
-  const { logout } = useUserApi(); 
-  
+  const { resetUser } = useUserStore();
+  const { logout, checkUnreadLetter } = useUserApi();
+  const location = useLocation();
+  const hasCompleteProfile = useHasCompleteProfile();
+
+  // 메뉴 상태 관리
+  const [isUnread, setIsUnread] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLDivElement>(null);
+
+  // 메뉴 토글 함수
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prev) => !prev);
+  }, []);
+
+  // 메뉴 닫기 함수
+  const closeMenu = useCallback(() => {
+    setIsMenuOpen(false);
+  }, []);
+
+  // 바깥 클릭 시 메뉴 닫기
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        buttonRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closeMenu]);
+
+  //  편지 상태
+  useEffect(() => {
+    if (hasCompleteProfile) {
+      if (location.pathname === '/storage') {
+        setIsUnread(false);
+      } else {
+        const checkForUnreadLetters = async () => {
+          try {
+            const letterResponse = await checkUnreadLetter();
+            if (letterResponse.status === 200) {
+              setIsUnread(letterResponse.data.isUnread);
+            }
+          } catch (letterError) {
+            console.error('읽지 않은 편지 확인 실패:', letterError);
+          }
+        };
+
+        checkForUnreadLetters();
+      }
+    }
+  }, [checkUnreadLetter, hasCompleteProfile, location.pathname]);
+
+  // 메인 경로인지 확인
+  const isMainPath = useCallback(() => {
+    return location.pathname === '/';
+  }, [location.pathname]);
+
+  // 경로 일치하는지 확인
+  const isActivePath = useCallback(
+    (path: string) => {
+      return location.pathname === path;
+    },
+    [location.pathname],
+  );
+
   // 로그아웃 처리
   const handleLogout = useCallback(async () => {
     try {
@@ -28,6 +102,14 @@ const useHeader = () => {
   }, [logout, resetUser]);
 
   return {
+    isMenuOpen,
+    menuRef,
+    buttonRef,
+    isUnread,
+    isMainPath,
+    isActivePath,
+    toggleMenu,
+    closeMenu,
     handleLogout,
   };
 };
