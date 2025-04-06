127b66c63289edf2211472a215f6e3db0fd7362d
김민주 <mintminju130@naver.com>
2025-04-06 11:57:06 +0900

    Design: 다크모드에서 input 흰 배경 검은 글씨로 변경
    S12P21D201-216

--- FE/src/components/album/PhotoAlbum.tsx
+++ FE/src/components/album/PhotoAlbum.tsx
@@ -436,7 +436,7 @@ function PhotoAlbum() {
                 }
               }}
               placeholder={albumTitle}
-              className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
             <div className="flex justify-end mt-1">
               <span
@@ -460,7 +460,7 @@ function PhotoAlbum() {
                 }
               }}
               placeholder={albumContent}
-              className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
             <div className="flex justify-end mt-1">
               <span
