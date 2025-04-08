cbcb559afe04bcd5103e871eff154b378af3e7a2
김민주 <mintminju130@naver.com>
2025-04-09 02:46:16 +0900

    Fix: 그룹 생성 시 이름 유효성 검증(공백) 추가
    S12P21D201-216

--- FE/src/pages/CreateGroupPage.tsx
+++ FE/src/pages/CreateGroupPage.tsx
@@ -54,16 +54,16 @@ function CreateGroupPage() {
         return
       }
 
-      if (groupName.length < 1) {
+      if (groupName.trim().length < 1) {
         setNameError("그룹명은 최소 1글자 이상이어야 합니다.")
         return
       }
 
-      if (groupName.length > 10) {
+      if (groupName.trim().length > 10) {
         setNameError("그룹명은 최대 10글자까지 가능합니다.")
         return
       }
-
+      setGroupName(groupName.trim());
       setCurrentStep(currentStep + 1)
     } else {
       if (!groupImage) {
